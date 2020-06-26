1ce669a84d49d9afa018166a672b21b55b2f1d5d
xiewenhua <vievenvua@gmail.com>
2020-06-26 20:08:56 +0800

    正常添加书籍正常，重复添加书籍不正常

--- app.py
+++ app.py
@@ -1,24 +1,49 @@
 from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask import request
+from flask import flash
+from flask import redirect
+from flask import url_for
 # import pymysql
 
 app = Flask(__name__)
 db_uri = "mysql+pymysql://root:root@localhost/books"
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'dev'
+
 
 @app.context_processor
 def infect_user():
-    books=Books.query.all()
+    books = Books.query.all()
     return dict(books=books)
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        bookname = request.form.get('bookname')
+        isbn = request.form.get('isbn')
+        score = request.form.get('score')
+        if not isbn:
+            print('-----  isbn is null -----')
+            flash("ISBN为必填项！")
+            return redirect(url_for('index'))
+        try:
+            book = Books(isbn=isbn, bookname=bookname, score=float(score))
+            db.session.add(book)
+        except:
+            return '------Add ERROR-----'
+        db.session.commit()
+        flash('添加成功！')
+        return redirect(url_for('index'))
+    print('####### is Get #######')
+
     return render_template('index.html')
 
 
 class Books(db.Model):
-    isbn=db.Column(db.String,primary_key=True)
-    bookname=db.Column(db.String)
-    score=db.Column(db.Float)
+    isbn = db.Column(db.String, primary_key=True)
+    bookname = db.Column(db.String)
+    score = db.Column(db.Float)
